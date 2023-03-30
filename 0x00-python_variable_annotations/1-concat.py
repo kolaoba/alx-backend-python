8235cf270851b09255b3f6343a3bccd000a35c81
Kolapo Obajulwa <Kolaobajulwa@gmail.com>
2023-03-30 18:08:15 +0100

    add task 1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,15 @@
+"""Type-annotated function concat that takes
+a string str1 and a string str2 as arguments
+and returns a concatenated string"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Returns the concatenation of two strings
+    
+    args:
+        str1: string
+        str2: string
+    """
+    
+    return str1 + str2
