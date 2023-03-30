cd275755baa213e29beac324e45abedef7a16b41
Kolapo Obajulwa <Kolaobajulwa@gmail.com>
2023-03-30 19:58:21 +0100

    lint 102

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -5,6 +5,7 @@ Use mypy to validate the following piece of code and apply any necessary changes
 
 from typing import Tuple, List
 
+
 def zoom_array(lst: Tuple, factor: int = 2) -> List:
     """zoom into array
 
