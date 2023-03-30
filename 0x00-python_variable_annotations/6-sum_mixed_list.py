b9691d8c00632d8c4bf23602e7deabae39672641
Kolapo Obajulwa <Kolaobajulwa@gmail.com>
2023-03-30 19:06:48 +0100

    add task 6

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Type-annotated function sum_mixed_list which takes
+a list mxd_lst of integers and floats and
+returns their sum as a float"""
+
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """
+    Returns the sum of a list of floats and integers
+
+    args:
+        mxd_lst: list of floats and integers
+    """
+
+    return sum(mxd_lst)
