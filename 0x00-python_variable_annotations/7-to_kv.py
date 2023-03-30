afa7bfdd0b5bbfcbe064e1d2ae3eebc52d30d435
Kolapo Obajulwa <Kolaobajulwa@gmail.com>
2023-03-30 19:13:18 +0100

    add tuple types

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -10,7 +10,7 @@ returns a tuple
 from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float]) -> Tuple:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """Returns a tuple of a string and a float
 
     Args:
