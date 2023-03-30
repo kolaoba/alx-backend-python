ec9981df450433e569c66284db599242e807d792
Kolapo Obajulwa <Kolaobajulwa@gmail.com>
2023-03-30 18:35:38 +0100

    add task 3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Type-annotated function to_str that takes
+a float n as argument and
+returns the string representation of the float.
+"""
+
+
+def to_str(n: float) -> str:
+    """Returns string representation of a float
+
+    Args:
+        n (float): float to be converted to string
+
+    Returns:
+        str: stringified float
+    """
+    return str(n)
