1b1d1ae4191500baf53284f7268e03868e190eb6
Kolapo Obajulwa <Kolaobajulwa@gmail.com>
2023-03-30 20:03:03 +0100

    lint task 101

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python3
-"""Given the parameters and the return values, add type annotations to the function"""
+"""Given the parameters and the return values,
+add type annotations to the function"""
 
 from typing import Union, Any, Mapping, TypeVar
 
 
-def safely_get_value(dct: Mapping, key: Any, default: Union[TypeVar, None] = None) -> Union[Any, TypeVar]:
+def safely_get_value(dct: Mapping,
+                     key: Any,
+                     default: Union[TypeVar,
+                                    None] = None) -> Union[Any,
+                                                           TypeVar]:
     """safely reutrn value of key in dict or default
 
     Args:
         dct (Mapping): dictionary to be checked
         key (Any): key to be checked
-        default (Union[TypeVar, None], optional): value to be retrieved. Defaults to None.
+        default (Union[TypeVar, None], optional): value to be retrieved.
+                                                Defaults to None.
 
     Returns:
         Union[Any, TypeVar]: value if exists or default
