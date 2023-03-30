66e35639e027e31802474f2ec310b9c7702b0152
Kolapo Obajulwa <Kolaobajulwa@gmail.com>
2023-03-30 18:56:05 +0100

    add task 5

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Type-annotated function sum_list which takes
+a list input_list of floats as argument and
+returns their sum as a float."""
+
+
+def sum_list(input_list: list(float)) -> float:
+    """
+    Returns the sum of a list of floats
+
+    args:
+        input_list: list of floats
+    """
+
+    return sum(input_list)
