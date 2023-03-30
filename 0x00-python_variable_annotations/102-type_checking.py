726ec42b263cdde980faa04f539182d43be33fd7
Kolapo Obajulwa <Kolaobajulwa@gmail.com>
2023-03-30 19:57:07 +0100

    add task 102

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+Use mypy to validate the following piece of code and apply any necessary changes.
+"""
+
+from typing import Tuple, List
+
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """zoom into array
+
+    Args:
+        lst (Tuple): array to be zoomed
+        factor (int, optional): zoom factor. Defaults to 2.
+
+    Returns:
+        zoomed_in (List): zoomed array
+    """
+    zoomed_in: List = [
+        item for item in lst
+        for i in range(factor)
+    ]
+    return zoomed_in
+
+
+array = (12, 72, 91)
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3)
