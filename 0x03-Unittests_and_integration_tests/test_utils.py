61fd8e6c3eb6b6e39378a1848d51eea90a67aafc
Kolapo Obajulwa <Kolaobajulwa@gmail.com>
2023-04-22 21:24:35 +0100

    add task 1

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -15,3 +15,11 @@ class TestAccessNestedMap(unittest.TestCase):
     ])
     def test_access_nested_map(self, nested_map, path, expected):
         assert access_nested_map(nested_map, path) == expected
+
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b"))
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        with self.assertRaises(KeyError):
+            access_nested_map(nested_map, path)
