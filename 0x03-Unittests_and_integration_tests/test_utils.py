44353fa87eb3e223a845a0715c62766cac39a223
Kolapo Obajulwa <Kolaobajulwa@gmail.com>
2023-04-22 21:14:37 +0100

    add task 0

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Test Access Nested Map Fn"""
+import unittest
+from parameterized import parameterized
+from utils import access_nested_map
+
+class TestAccessNestedMap(unittest.TestCase):
+    """Defines TestAccessNestedMap Class"""
+    
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+    ])
+    def test_access_nested_map(self, nested_map, path, expected):
+        assert access_nested_map(nested_map, path) == expected
