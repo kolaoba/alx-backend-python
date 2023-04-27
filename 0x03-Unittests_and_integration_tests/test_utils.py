3348747da4d04863b649d2e69fbed61541180faf
Kolapo Obajulwa <Kolaobajulwa@gmail.com>
2023-04-27 22:41:44 +0100

    double quotes to single

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -9,9 +9,9 @@ class TestAccessNestedMap(unittest.TestCase):
     """Defines TestAccessNestedMap Class"""
 
     @parameterized.expand([
-        ({"a": 1}, ("a",), 1),
-        ({"a": {"b": 2}}, ("a",), {"b": 2}),
-        ({"a": {"b": 2}}, ("a", "b"), 2)
+        ({'a': 1}, ('a',), 1),
+        ({'a': {'b': 2}}, ('a',), {'b': 2}),
+        ({'a': {'b': 2}}, ('a', 'b'), 2)
     ])
     def test_access_nested_map(self, nested_map, path, expected):
         """test access_nested_map function"""
