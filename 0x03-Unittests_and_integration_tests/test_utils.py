8ac50314e7274e86dcd5e085c85d9469ccf3d8c8
Kolapo Obajulwa <Kolaobajulwa@gmail.com>
2023-04-27 22:53:56 +0100

    add memoize

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,7 +2,7 @@
 """Test Access Nested Map Fn"""
 import unittest
 from parameterized import parameterized
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 from unittest import mock
 
 
@@ -41,3 +41,23 @@ class TestGetJson(unittest.TestCase):
             get_data.return_value = res
             self.assertEqual(get_json(test_url), test_payload)
 
+class TestMemoize(unittest.TestCase):
+    """Defines TestMemoize class"""
+
+    def test_memoize(self):
+        """test memoize function"""
+        class TestClass:
+
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+            
+        with mock.patch.object(TestClass, "a_method",
+                               return_value=lambda: 42) as func:
+            test_obj = TestClass()
+            self.assertEqual(test_obj.a_property(), 42)
+            self.assertEqual(test_obj.a_property(), 42)
+            func.assert_called_once()
