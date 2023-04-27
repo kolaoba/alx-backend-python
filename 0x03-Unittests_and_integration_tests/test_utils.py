9310e11c40142b8970a57a89ec7628ada2b096ee
Kolapo Obajulwa <Kolaobajulwa@gmail.com>
2023-04-27 22:37:55 +0100

    add task o method docs

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -14,6 +14,7 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
     def test_access_nested_map(self, nested_map, path, expected):
+        """test access_nested_map function"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
     @parameterized.expand([
