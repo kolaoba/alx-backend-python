e203d59ea66e1f016cbf2851a4b4ce6c3f77b615
Kolapo Obajulwa <Kolaobajulwa@gmail.com>
2023-04-27 22:35:01 +0100

    fix task 0

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -14,7 +14,7 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
     def test_access_nested_map(self, nested_map, path, expected):
-        assert access_nested_map(nested_map, path) == expected
+        self.assertEqual(access_nested_map(nested_map, path), expected)
 
     @parameterized.expand([
         ({}, ("a",)),
